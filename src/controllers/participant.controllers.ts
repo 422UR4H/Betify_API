9af55ef8adaf305d5c05c3f94ae9c85cc2183b25
422UR4H <tiagosindra@id.uff.br>
2023-11-13 08:40:59 -0300

    :recycle: Refactor: participant

--- src/controllers/participant.controllers.ts
+++ src/controllers/participant.controllers.ts
@@ -1,19 +1,18 @@
 import { Request, Response } from 'express';
 import { InputParticipantDto } from '@/protocols/participant.protocols';
-import ParticipantService from '@/services/participant.services';
+import participantService from '@/services/participant.services';
 import httpStatus from 'http-status';
-import { Participant } from '@prisma/client';
 
 async function findAll(_req: Request, res: Response) {
-  const participants = await ParticipantService.findAll();
+  const participants = await participantService.findAll();
   return res.send(participants);
 }
 
 async function create(req: Request, res: Response) {
   const inputParticipantDto = req.body as InputParticipantDto;
-  const participant = await ParticipantService.create(inputParticipantDto);
+  const participant = await participantService.create(inputParticipantDto);
   return res.status(httpStatus.CREATED).send(participant);
 }
 
-const ParticipantController = { findAll, create };
-export default ParticipantController;
+const participantController = { findAll, create };
+export default participantController;
--- src/routers/participant.routes.ts
+++ src/routers/participant.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import validateSchema from '@/middlewares/validateSchema';
 import { participantSchema } from '@/schemas/participants.schemas';
-import ParticipantController from '@/controllers/participant.controllers';
+import participantController from '@/controllers/participant.controllers';
 
 const participantRouter = Router();
 
-participantRouter.get('/participants', ParticipantController.findAll);
-participantRouter.post('/participants', validateSchema(participantSchema), ParticipantController.create);
+participantRouter.get('/participants', participantController.findAll);
+participantRouter.post('/participants', validateSchema(participantSchema), participantController.create);
 export default participantRouter;
