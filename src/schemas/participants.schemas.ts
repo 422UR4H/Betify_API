abbe21274dbd941d0ea891fa2b9f0cd8bb18fce5
422UR4H <tiagosindra@id.uff.br>
2023-11-13 04:45:25 -0300

    :bug: Fix: max int balance

--- src/schemas/participants.schemas.ts
+++ src/schemas/participants.schemas.ts
@@ -1,6 +1,7 @@
+import { MAX_INT_32 } from '@/utils/constants.utils';
 import Joi from 'joi';
 
 export const participantSchema = Joi.object({
   name: Joi.string().min(3).max(64).required(),
-  balance: Joi.number().integer().min(1).required(),
+  balance: Joi.number().integer().min(1).max(MAX_INT_32).required(),
 });
