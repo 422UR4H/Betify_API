5bdd09f04164d3dff77c953927a91c3b6d06b255
422UR4H <tiagosindra@id.uff.br>
2023-11-20 16:33:31 -0300

    :bulb: Comment: FinishBetDto

--- src/protocols/bet.protocols.ts
+++ src/protocols/bet.protocols.ts
@@ -4,5 +4,14 @@ import { SystemData } from './participant.protocols';
 export type InputBetDto = Omit<Bet, SystemData | 'id' | 'status' | 'amountWon'>;
 export type CreateBetDto = Omit<Bet, SystemData | 'id' | 'amountWon'>;
 export type OutputBetDto = Bet;
-export type FinishBetDto = Pick<Bet, 'id' | 'amountWon' | 'status'>;
 export type FinishParticipantDto = Pick<Bet, 'participantId' | 'amountWon'>;
+export type FinishBetDto = Pick<Bet, 'id' | 'participantId' | 'amountWon' | 'status'>;
+
+// export class FinishBetDto {
+//   constructor(
+//     public id: string,
+//     public participantId: number,
+//     public amountWon: number,
+//     public status: Status,
+//   ) {}
+// }
