350c4db6f8cbe02a44d2c8caf415d36b9f00ec0d
422UR4H <tiagosindra@id.uff.br>
2023-11-21 14:46:42 -0300

    :bulb: Docs: remove test comments

--- tests/integration/health.test.ts
+++ tests/integration/health.test.ts
@@ -7,8 +7,6 @@ const api = supertest(app);
 describe('GET /health', () => {
   it("should return I'm OK! and 200 status code", async () => {
     const { status, text } = await api.get('/health');
-    // const result = await api.get('/health');
-    // console.log(result)
     expect(status).toBe(httpStatus.OK);
     expect(text).toBe("I'm OK!");
   });
