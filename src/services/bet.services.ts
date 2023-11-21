311511bc4d6c8841c8c68b925a28c9e5d752d7ec
422UR4H <tiagosindra@id.uff.br>
2023-11-21 14:37:02 -0300

    :bulb: Docs: remove test comments

--- src/services/bet.services.ts
+++ src/services/bet.services.ts
@@ -9,13 +9,11 @@ async function createAndLiquidadePayment(bet: InputBetDto): Promise<Bet> {
   const { participantId, amountBet } = bet;
 
   const { balance } = await participantService.findById(participantId);
-  // FIXME: unit test here
   if (balance < amountBet) {
     throw customErrors.forbidden("Participant doesn't have enough balance");
   }
 
   const game = await gameService.findById(bet.gameId);
-  // FIXME: unit test here
   if (game.isFinished) throw customErrors.gone('This game is already over');
 
   const createBetDto: CreateBetDto = { ...bet, status: Status.PENDING };
