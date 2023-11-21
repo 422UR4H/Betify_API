8bfddc19f2e6d25472ab52cd3b6f8ca8460d08ff
422UR4H <tiagosindra@id.uff.br>
2023-11-21 12:46:00 -0300

    :recycle: Refactor: magic numbers

--- src/schemas/participants.schemas.ts
+++ src/schemas/participants.schemas.ts
@@ -1,8 +1,8 @@
 import { InputParticipantDto } from '@/protocols/participant.protocols';
-import { MAX_INT_32 } from '@/utils/constants.utils';
+import { MAX_INT_32, MIN_PARTICIPANT_BALANCE } from '@/utils/constants.utils';
 import Joi from 'joi';
 
 export const participantSchema = Joi.object<InputParticipantDto>({
   name: Joi.string().min(3).max(64).required(),
-  balance: Joi.number().integer().min(1000).max(MAX_INT_32).required(),
+  balance: Joi.number().integer().min(MIN_PARTICIPANT_BALANCE).max(MAX_INT_32).required(),
 });
--- src/utils/constants.utils.ts
+++ src/utils/constants.utils.ts
@@ -1,2 +1,3 @@
 export const MAX_INT_32 = 2147483647; // 0x7FFFFFFF || 2^31 - 1
 export const HOUSE_FEE = 0.3;
+export const MIN_PARTICIPANT_BALANCE = 1000;
