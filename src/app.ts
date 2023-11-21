0eed7d7f7be0de6d9bd91fa0ad3473556b8c8cef
422UR4H <tiagosindra@id.uff.br>
2023-11-21 14:51:29 -0300

    :lock: Fix: use helmet

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,11 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import router from '@/routers/index.routes';
 import errorHandler from '@/middlewares/errorHandler';
+import helmet from 'helmet';
 
 dotenv.config();
 
 const app = express();
-app.use(json()).use(cors()).use(router).use(errorHandler);
+app.use(helmet()).use(json()).use(cors()).use(router).use(errorHandler);
 
 export default app;
